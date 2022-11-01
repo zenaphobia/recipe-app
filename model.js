0746b947e091064e331a7c365788bd253e6f818e
zenaphobia <100097619+zenaphobia@users.noreply.github.com>
2022-10-31 20:35:20 -0700

    adding all files

---
+++ model.js
@@ -0,0 +1,7 @@
+let recipeContainer, recipeName, recipeIngredients, recipeInstructions;
+
+function renderRecipe( _elementId, _recipeObject ){
+    //webPage = document.body();
+    recipeContainer = document.getElementById( _elementId );
+
+}
